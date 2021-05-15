74e348cbf31f1d038c10b047d294ccf3ef3a36d3
CW <48869711+Dondonn@users.noreply.github.com>
2021-05-15 16:46:19 +0200

    Update readfasta.py

--- readfasta.py
+++ readfasta.py
@@ -1,30 +1,34 @@
+
 def readfasta(seq_file, labels_file):
     #seq_file = input('please enter the sequence file path')
     #labels_file = input('please enter the labals file path')
     seq_d = {}
-    with open(seq_file, 'r',newline='') as reader: # read seqdata
+    seq_number = 0
+    label_number =0
+    with open(seq_file, 'r',newline='') as reader:
         seq_num = ''
         for line in reader.readlines():
             line = line.rstrip()
             if line.startswith('>'):
                 seq_num = line[1:]
+                seq_number+=1
             else:
                 seq_d[seq_num] = [line]
- 
+        #print(seqnum_d)
 
-    with open(labels_file, 'r',newline='') as reader: # read labels data
+    with open(labels_file, 'r',newline='') as reader:
         label_num = ''
         for line in reader.readlines():
             line = line.rstrip()
             if line.startswith('>'):
+                label_number+=1
                 label_num = line[1:]
             else:
                 if label_num in seq_d:
                     seq_d[label_num].append(line)
                 else:
                     seq_d[label_num] = ['', line]
-    print("Read Done!")
+    print("Read Done With %s Reads and %s Labels" %(seq_number,label_number))
     return seq_d
-
 if __name__ == "__main__":
-    merged = readfasta("data\\disorder_seq.fasta","data\\disorder_labels.fasta")
+    merged = readfasta("D:\\Study thingie\\FACH\\S4 WS2021\\PP\\uebung\\data\\disorder_seq.fasta","D:\\Study thingie\\FACH\\S4 WS2021\\PP\\uebung\\data\\disorder_labels.fasta")
