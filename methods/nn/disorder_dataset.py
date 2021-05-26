fixed dataloader made z-scores into protein-wise averages (to make development of a test CNN easier)

methods/nn/disorder_dataset.py
@@ -11,6 +11,7 @@ from torch.nn.utils.rnn import pad_sequence
 
 project_root = os.getcwd()
 
+
 class DisorderDataset(Dataset):
 
     def __init__(self, x, y):
@@ -24,7 +25,7 @@ class DisorderDataset(Dataset):
         """
 
         self.x = x
-        self.y = y
+        self.y = self.calculate_avg_z(y)
         self.avg_y = self.calculate_avg_z(y)
         self.aa_len = np.array([len(seq) for seq in x])
         self.bins = self.assign_bins(self.avg_y)
@@ -43,12 +44,12 @@ class DisorderDataset(Dataset):
         embeddings = self.x[i]
         z_scores = self.y[i]
 
-        return {
-            'embeddings': torch.tensor(embeddings).float(),
-            'z_scores': torch.tensor(z_scores).float(),
-        }
+        # return {
+        #     'embeddings': torch.tensor(embeddings).float(),
+        #     'z_scores': torch.tensor(z_scores).float(),
+        # }
+        return [torch.tensor(embeddings).float(), torch.tensor(z_scores).float()]
 
-    
     '''
     Calculates an average z_score per aa sequence. Unknown z-scores are
     ignored.
@@ -61,11 +62,10 @@ class DisorderDataset(Dataset):
             avg_zs: list, list of averaged z-scores per sequence 
     '''
 
-    def calculate_avg_z(self,y):
+    def calculate_avg_z(self, y):
 
         unknown_z_value = 999.0
         avg_zs = []
-
 
         for z_scores_per_seq in y:
             # seq length after removing unknown z-scores 
@@ -79,10 +79,8 @@ class DisorderDataset(Dataset):
                     trimmed_length += 1
                     total_z_sum += z_score
 
-            #avg_zs.append(total_z_sum/trimmed_length)
-            
-            avg_zs.append(total_z_sum)
-            
+            avg_zs.append(total_z_sum/trimmed_length)
+
         return np.array(avg_zs)
 
     ''' Assign bins (int class indicator) to each of samples according
@@ -95,7 +93,7 @@ class DisorderDataset(Dataset):
             assigned_bins: list of integers indicating an artificial class label
          '''
 
-    def assign_bins(self,avg_z, bins_num=4):
+    def assign_bins(self, avg_z, bins_num=4):
         samples_per_bin, bins, = np.histogram(avg_z, bins=4)
         print("Samples per bin", samples_per_bin)
         print("Thresholds of bins", bins)
@@ -115,63 +113,39 @@ class DisorderDataset(Dataset):
                 break
         return np.array(assigned_bins)
 
-    
-
 
 def load_dataset(path=os.path.join(project_root, "data")):
-
     z_score_path = 'baseline_embeddings_disorder.h5'
 
     labels_path = 'disorder_labels.fasta'
 
-    x,y = read_data(os.path.join(path, z_score_path), 
-                    os.path.join(path, labels_path))
-    new_x = []
-    
-    test = 23
-    
-    for protein in x:
-        new_x.append(protein[0:test])
-    x = new_x
+    x, y = read_data(os.path.join(path, z_score_path),
+                     os.path.join(path, labels_path))
 
-    new_y = []
-    
-    for z_score in y:
-        new_y.append(z_score[0:test])
-    y = new_y
-    
-    
-    dataset = DisorderDataset(x,y)
+    dataset = DisorderDataset(x, y)
 
     return dataset
 
 
 def create_dataframe(path=os.path.join(project_root, "data")):
-    
     z_score_path = 'baseline_embeddings_disorder.h5'
 
     labels_path = 'disorder_labels.fasta'
 
-    x,y = read_data(os.path.join(path, z_score_path), 
-                    os.path.join(path, labels_path))
+    x, y = read_data(os.path.join(path, z_score_path),
+                     os.path.join(path, labels_path))
 
     df = pd.DataFrame({'x': x, 'y': y})
 
     return df
+
 
 def collate(batch):
     """
         To be passed to DataLoader as the `collate_fn` argument
     """
     assert isinstance(batch, list)
-    data = pad_sequence([b['embeddings'] for b in batch], batch_first = True)
-    lengths = torch.tensor([len(b['embeddings']) for b in batch])
-    padded_batches = pad_sequence([b['z_scores'] for b in batch], padding_value=999.0, batch_first = True)
-    label = torch.stack([p for p in padded_batches])
-    
-    print(f"Size of padded data {data.size()}")
-    return {
-        'embeddings': data,
-        'z_scores': label,
-        'lengths': lengths
-    }
+    data = [item[0] for item in batch]
+    target = [item[1] for item in batch]
+
+    return [data, target]

methods/nn/model.py
@@ -4,30 +4,31 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 
+
+
 class FeedForwardNet(pl.LightningModule):
-    
+
     def __init__(self, hparams):
         super().__init__()
-        
+
         # set hyperparams
         self.hparams = hparams
-        
-        
-        self.model = None 
+
+        self.model = None
 
         ########################################################################
         # TODO: Initialize your model!                                         #
         ########################################################################
-        
+
         embedding_dim = 1024
         longest_seq = 23
-        input_size=longest_seq*embedding_dim
-        output_size=longest_seq
-        
+        input_size = longest_seq * embedding_dim
+        output_size = longest_seq
+
         self.model = nn.Sequential(
-          nn.Linear(input_size, self.hparams['hidden_size']),
-          nn.ReLU(),
-          nn.Linear(self.hparams['hidden_size'], output_size)
+            nn.Linear(input_size, self.hparams['hidden_size']),
+            nn.ReLU(),
+            nn.Linear(self.hparams['hidden_size'], output_size)
         )
 
         ########################################################################
@@ -35,33 +36,30 @@ class FeedForwardNet(pl.LightningModule):
         ########################################################################
 
     def forward(self, x):
-
         # x.shape =  [batch_size, seq_length, 1024] -> flatten first
-        
+
         x = x.view(x.shape[0], -1)
 
         # feed x into model
         x = self.model(x)
 
         return x
-    
-    def general_step(self, batch, batch_idx, mode):
 
+    def general_step(self, batch, batch_idx, mode):
         # forward pass
         out = self.forward(batch['embeddings'])
 
         # loss
         targets = batch['z_scores']
-        
-        #print(out.shape)
-        #print(targets.shape)
+
+        # print(out.shape)
+        # print(targets.shape)
 
         loss = nn.MSELoss()
-        loss = loss(out,targets)
-        
-        
+        loss = loss(out, targets)
+
         return loss
-    
+
     def general_end(self, outputs, mode):
         # average over all batches aggregated during one epoch
         avg_loss = torch.stack([x[mode + '_loss'] for x in outputs]).mean()
@@ -75,7 +73,7 @@ class FeedForwardNet(pl.LightningModule):
     def validation_step(self, batch, batch_idx):
         loss = self.general_step(batch, batch_idx, "val")
         return {'val_loss': loss}
-    
+
     def test_step(self, batch, batch_idx):
         loss = self.general_step(batch, batch_idx, "test")
         return {'test_loss': loss}
@@ -88,6 +86,7 @@ class FeedForwardNet(pl.LightningModule):
     def configure_optimizers(self):
         optim = torch.optim.Adam(self.model.parameters(), self.hparams["learning_rate"])
         return optim
+
 
 '''    def getTestAcc(self, loader = None):
         self.model.eval()
@@ -111,3 +110,110 @@ class FeedForwardNet(pl.LightningModule):
         preds = scores.argmax(axis=1)
         acc = (labels == preds).mean()
         return preds, acc'''
+
+
+class ConvNet(pl.LightningModule):
+    """Facial keypoint detection model"""
+
+    def __init__(self, hparams):
+        """
+        Initialize your model from a given dict containing all your hparams
+        Warning: Don't change the  method declaration (i.e. by adding more
+            arguments), otherwise it might not work on the submission server
+        """
+        super(ConvNet, self).__init__()
+        self.hparams = hparams
+        ########################################################################
+        # TODO: Define all the layers of your CNN, the only requirements are:  #
+        # 1. The network takes in a batch of images of shape (Nx1x96x96)       #
+        # 2. It ends with a linear layer that represents the keypoints.        #
+        # Thus, the output layer needs to have shape (Nx30),                   #
+        # with 2 values representing each of the 15 keypoint (x, y) pairs      #
+        #                                                                      #
+        # Some layers you might consider including:                            #
+        # maxpooling layers, multiple conv layers, fully-connected layers,     #
+        # and other layers (such as dropout or batch normalization) to avoid   #
+        # overfitting.                                                         #
+        ########################################################################
+
+        self.model = nn.Sequential(
+            #nn.Conv2d(1024, 60, (3, 3), stride=1, padding=2),
+            nn.Conv2d(1024, 60, (3, 3)),
+            nn.PReLU(),
+            # nn.MaxPool2d(3),
+            # nn.Conv2d(32, 64, (3, 3), stride=1, padding=2),
+            # nn.PReLU(),
+            # nn.MaxPool2d(2, stride=2),
+            # nn.Conv2d(64, 128, (2, 2), stride=1, padding=1),
+            # nn.PReLU(),
+            # #Flatten(),
+            # # issue here: linear layers require the most parameters
+            # nn.Linear(10368, 256),
+            # nn.Dropout(0.1),
+            # nn.PReLU(),
+            nn.Linear(256, 30)
+        )
+
+        print(self.model)
+
+        ########################################################################
+        #                           END OF YOUR CODE                           #
+        ########################################################################
+
+    def forward(self, x):
+        ########################################################################
+        # TODO: Define the forward pass behavior of your model                 #
+        # for an input image x, forward(x) should return the                   #
+        # corresponding predicted keypoints                                    #
+        ########################################################################
+
+        print("FORWARD PASS")
+        print(x.size())
+        x = self.model(x)
+
+        ########################################################################
+        #                           END OF YOUR CODE                           #
+        ########################################################################
+        return x
+
+    def general_step(self, batch, batch_idx, mode):
+        # forward pass
+        out = self.forward(batch[0])
+
+        # loss
+        targets = batch[1]
+
+        # print(out.shape)
+        # print(targets.shape)
+
+        loss = nn.MSELoss()
+        loss = loss(out, targets)
+
+        return loss
+
+    def general_end(self, outputs, mode):
+        # average over all batches aggregated during one epoch
+        avg_loss = torch.stack([x[mode + '_loss'] for x in outputs]).mean()
+        return avg_loss
+
+    def training_step(self, batch, batch_idx):
+        loss = self.general_step(batch, batch_idx, "train")
+        tensorboard_logs = {'loss': loss}
+        return {'loss': loss, 'log': tensorboard_logs}
+
+    def validation_step(self, batch, batch_idx):
+        loss = self.general_step(batch, batch_idx, "val")
+        return {'val_loss': loss}
+
+    def test_step(self, batch, batch_idx):
+        loss = self.general_step(batch, batch_idx, "test")
+        return {'test_loss': loss}
+
+    def validation_end(self, outputs):
+        avg_loss = self.general_end(outputs, "val")
+        tensorboard_logs = {'val_loss': avg_loss}
+        return {'val_loss': avg_loss, 'log': tensorboard_logs}
+
+    def configure_optimizers(self):
+        optim = torch.optim.Adam(self.model.parameters(), self.hparams["learning_rate"])
+        return optim