b92089471630f6e776a5c0e1dd662011a5c4acaa
stelviopas <38905134+stelviopas@users.noreply.github.com>
2021-06-16 17:10:56 +0200

    Add test logs to tensor board

--- methods/nn/ffnet.py
+++ methods/nn/ffnet.py
@@ -8,12 +8,18 @@ from methods.nn.disorder_dataset import load_dataset
 
 class FFNet(pl.LightningModule):
 
-    def __init__(self, hparams):
+    def __init__(self, hparams, *args, **kwargs):
         """
         Initialize your model from a given dict containing all your hparams
         """
         super().__init__()
         self.hparams = hparams
+        self.hidden_size = hparams["hidden_size"]
+        self.learning_rate = hparams["learning_rate"]
+        self.window_size = hparams["window_size"]
+        self.batch_size = hparams["batch_size"]
+        self.test_results = 0.0
+
         ########################################################################
         # TODO: Define all the layers of your Feed Forward Network
         ########################################################################
@@ -22,11 +28,11 @@ class FFNet(pl.LightningModule):
             # input: batch_size * 1024 * window_size
             nn.Flatten(),
             # here, we have 1024 * window_size
-            nn.Linear(1024 * hparams['window_size'], 128),
-            nn.PReLU(),
-            nn.Linear(128, 64),
+            nn.Linear(1024 * self.window_size, self.hidden_size),
+            nn.ReLU(),
+            nn.Linear(self.hidden_size, 32),
             nn.Sigmoid(),
-            nn.Linear(64, 1),
+            nn.Linear(32, 1),
         )
 
         print(self.model)
@@ -61,42 +67,41 @@ class FFNet(pl.LightningModule):
 
         loss = nn.MSELoss()
         loss = loss(out, targets)
-
-
         return loss
+
+    def training_step(self, batch, batch_idx):
+        loss = self.general_step(batch, batch_idx, "train")
+        tensorboard_logs = {'train_loss': loss}
+        return {'loss': loss, 'log': tensorboard_logs}
+
+    def validation_step(self, batch, batch_idx):
+        loss = self.general_step(batch, batch_idx, "val")
+        tensorboard_logs = {'val_loss': loss}
+        return {'val_loss': loss, 'log': tensorboard_logs}
+
+    def test_step(self, batch, batch_idx):
+        loss = self.general_step(batch, batch_idx, "test")
+        tensorboard_logs = {'test_loss': loss}
+        return {'test_loss': loss, 'log': tensorboard_logs}
+
 
     def general_end(self, outputs, mode):
         # average over all batches aggregated during one epoch
         avg_loss = torch.stack([x[mode + '_loss'] for x in outputs]).mean()
         return avg_loss
-
-    def training_step(self, batch, batch_idx):
-        loss = self.general_step(batch, batch_idx, "train")
-        tensorboard_logs = {'loss': loss}
-        return {'loss': loss, 'log': tensorboard_logs}
-
-    def validation_step(self, batch, batch_idx):
-        loss = self.general_step(batch, batch_idx, "val")
-        return {'val_loss': loss}
-
-    def test_step(self, batch, batch_idx):
-        loss = self.general_step(batch, batch_idx, "test")
-        return {'test_loss': loss}
+    
+    def test_end(self, outputs):
+        avg_loss = self.general_end(outputs, "test")
+        tensorboard_logs = {'avg_test_loss': avg_loss}
+        self.test_results = avg_loss
+        return {'test_loss': avg_loss, 
+        'log': tensorboard_logs}
 
     def validation_end(self, outputs):
         avg_loss = self.general_end(outputs, "val")
-        tensorboard_logs = {'val_loss': avg_loss}
+        tensorboard_logs = {'avg_val_loss': avg_loss}
         return {'val_loss': avg_loss, 'log': tensorboard_logs}
 
     def configure_optimizers(self):
         optim = torch.optim.Adam(self.model.parameters(), self.hparams["learning_rate"])
         return optim
-
-
-if __name__ == "__main__":
-    hparams = {'hidden_size': 112,
-               "learning_rate": 1e-4,
-               'window_size': 15
-               }
-    dataset = load_dataset(path="../../../pp1cb_ss21/data", window_size=hparams['window_size'])
-    nested_cross_validation(dataset, mode='evaluate', model=FFNet(hparams=hparams))
